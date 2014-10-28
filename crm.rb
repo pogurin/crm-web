e24550a28a83de2ef9f06a09207ba05b6aa3f674
MitsuyoshiToguri <seiun340066@gmail.com>
2014-10-28 14:34:38 -0400

    This is second commitment

--- crm.rb
+++ crm.rb
@@ -1,4 +1,14 @@
+require_relative 'contact'
 require 'sinatra'
 get '/' do
-	"Main Menu"
+	@crm_app_name = "My CRM"
+	erb :index
+end
+
+get "/contacts" do
+	@contacts = []
+	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+	erb :contacts
 end
